82992806e3bffc0bebe594d4c4088d74efdfeb2c
thorsanchez <ths281@hi.is>
2026-01-06 13:04:59 +0000

    eyða rows og skipa nafn og iban numer

--- src/utgjoldyfirlit.py
+++ src/utgjoldyfirlit.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-df = pd.read_excel('data/AccountTransactions_0370-26-030101_42569080.xlsx')
-print(df.head())
+def lesa_gogn():
+    df = pd.read_excel('data/arionbanki.xlsx', header=2) #skipa fyrstu 2 raðir
+    df = df.dropna(how='all') #eyða auðar línar
+    return df
+
+df = lesa_gogn()
+print(df)
