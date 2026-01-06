7aff0f92bce45ed0dda6144f2710ec3cd6d51ed0
thorsanchez <ths281@hi.is>
2026-01-06 15:58:01 +0000

    notar gemini til að categorize-a explanation/nafn viðtakanda

--- src/utgjoldyfirlit.py
+++ src/utgjoldyfirlit.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import json
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def lesa_gogn():
     df = pd.read_excel('data/arionbanki.xlsx', header=3) #skipa fyrstu 2 raðir
@@ -37,8 +43,66 @@ def top_vidtakendur(df,n):
     topN = grouped.nsmallest(n, 'Amount')
     fjarslur_per_vidtakandi = utgjold_radir['Explanation'].value_counts()
     topN['Fjöldi færslna'] = topN['Explanation'].map(fjarslur_per_vidtakandi)
-
     return topN
+
+def flokka_med_gemini(faerslu_listi):
+    """
+    Þar sem "Nafn viðtakanda eða greiðanda" dálkur er messy þá ætlar gemini að categorize-a
+    """
+
+    gjold = df[df['Amount'] < 0].copy()
+    gjold = gjold.head(5)
+    #bua til dictionaries
+    faerslu_listi = gjold[['Date', 'Amount', 'Explanation']].to_dict('records')
+
+    transaction_text = "\n".join([
+        f"{t['Date']}: {t['Amount']} kr - {t['Explanation']}"
+        for t in faerslu_listi
+    ])
+
+    prompt = f"""
+    Þú ert fjármálasérfræðingur. Flokkaðu þessar færslur í flokka.
+    
+    Flokkar:
+    - Matur: matvörur, veitingastaðir, kaffihús
+    - Húsnæði: húsaleiga, veitur, íbúðafélag
+    - Samgöngur: bensín, strætó, bílaleiga
+    - Afþreying: bíó, netflix, íþróttir
+    - Fatnaður: föt, skór
+    - Heilsa: apótek, læknar
+    - Reikningar: sími, internet, tryggingar
+    - Millifærslur: til annarra einstaklinga
+    - Annað: allt annað
+    
+    Færslur:
+    {transaction_text}
+    
+    Svaraðu BARA með JSON:
+    [
+        {{"explanation": "BONUS FISKISLOD", "category": "Matur", "confidence": 0.95}},
+        ...
+    ]
+    """
+    
+    try:
+        client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+        
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+            config=genai.types.GenerateContentConfig(
+                response_mime_type='application/json',
+                temperature=0.3
+            )
+        )
+        
+        categories = json.loads(response.text)
+        return categories
+        
+    except Exception as e:
+        print(f"villa i api request: {e}")
+        return []
+
 
 df = lesa_gogn()
 #print(df.head)
@@ -46,4 +110,5 @@ df = lesa_gogn()
 #print(df.head())
 #print(utgjold_yfirlit(df))
 #print(top5_utgjold(df))
-print(top_vidtakendur(df,10))
+#print(top_vidtakendur(df,10))
+print(flokka_med_gemini(df))
